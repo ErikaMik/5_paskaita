cf9ae82e1f903e4f3eb8d2d5d015a1701146c1d7
ErikaMik <erikamik666@yahoo.com>
2018-10-10 22:40:40 +0300

    5 paskaitos originalas

---
+++ app.js
@@ -0,0 +1,14 @@
+function generate(qty) {
+	var right =document.getElementById('right'); //pointeris
+	right.innerHTML = '';
+for (var i = 0; i < qty; i++) {
+	var r = Math.round(Math.random() * 255);
+	var g = Math.round(Math.random() * 255);
+	var b = Math.round(Math.random() * 255);
+	right.innerHTML += '<div class="square" style="background-color: rgb('+r+','+g+','+b+')">' + i + '</div>';
+}
+}
+
+function show_items() {
+	console.log
+}
