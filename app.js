92293389d3e06c6c9be728647223446ff337a765
ErikaMik <erikamik666@yahoo.com>
2018-10-10 23:11:21 +0300

    Pataisos 1
    Langeliai skaiciuojami nuo 1

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function generate(qty) {
 	var right =document.getElementById('right'); //pointeris
-	right.innerHTML = '';
-for (var i = 0; i < qty; i++) {
+	right.innerHTML = ''; //clear 'right'
+for (var i = 1; i < qty; i++) {
 	var r = Math.round(Math.random() * 255);
 	var g = Math.round(Math.random() * 255);
 	var b = Math.round(Math.random() * 255);
